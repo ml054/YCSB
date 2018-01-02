9d3a63b0d772f26f240938096691db1d6e06b18b
Marcin Lewandowski <marcin@ravendb.net>
2018-01-02 17:15:43 +0100

    basic ravendb impl

--- ravendb/src/main/java/com/yahoo/ycsb/db/RavenDBClient.java
+++ ravendb/src/main/java/com/yahoo/ycsb/db/RavenDBClient.java
@@ -24,49 +24,229 @@
 
 package com.yahoo.ycsb.db;
 
-import com.yahoo.ycsb.ByteIterator;
-import com.yahoo.ycsb.DB;
-import com.yahoo.ycsb.DBException;
-import com.yahoo.ycsb.Status;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yahoo.ycsb.*;
+import net.ravendb.client.Constants;
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.commands.*;
+import net.ravendb.client.documents.queries.IndexQuery;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Vector;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * YCSB binding for RavenDB.
  */
 public class RavenDBClient extends DB {
 
+  public static final String RAVENDB_URL = "ravendb.url";
+
+  public static final String RAVENDB_DATABASE = "ravendb.database";
+
+  private static DocumentStore store;
+  private static ObjectMapper mapper;
+
+  public static DocumentStore getStore() {
+    return store;
+  }
+
+  /**
+   * Count the number of times initialized to teardown on the last
+   * {@link #cleanup()}.
+   */
+  private static final AtomicInteger INIT_COUNT = new AtomicInteger(0);
+
   @Override
   public void init() throws DBException {
-    super.init();
-    //TODO: do some init
+    INIT_COUNT.incrementAndGet();
+    synchronized (RavenDBClient.class) {
+      if (store != null) {
+        return;
+      }
+
+      Properties props = getProperties();
+
+      String urls = props.getProperty(RAVENDB_URL, null);
+      if (urls == null) {
+        urls = "http://localhost:8080";
+      }
+
+      String databaseName = props.getProperty(RAVENDB_DATABASE, "ycsb");
+
+      try {
+        DocumentStore documentStore = new DocumentStore(urls.split(","), databaseName);
+        documentStore.initialize();
+
+        System.out.println("ravendb client connection created with " + urls);
+        store = documentStore;
+        mapper = store.getConventions().getEntityMapper();
+      } catch (Exception e1) {
+        System.err
+            .println("Could not initialize RavenDB store:"
+                + e1.toString());
+        e1.printStackTrace();
+        return;
+      }
+    }
   }
 
   @Override
+  public void cleanup() throws DBException {
+    if (INIT_COUNT.decrementAndGet() == 0) {
+      try {
+        store.close();
+      } catch (Exception e1) {
+        System.err.println("Could not close RavenDB document store: "
+            + e1.toString());
+        e1.printStackTrace();
+        return;
+      } finally {
+        store = null;
+      }
+    }
+  }
+
+  private String documentId(String table, String key) {
+    return table + "/" + key;
+  }
+
+  //TODO: client side projection vs server side projection?
+  @Override
   public Status read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
-    return null;
+    try {
+      String documentId = documentId(table, key);
+
+      GetDocumentsCommand command = new GetDocumentsCommand(documentId, null, false);
+      store.getRequestExecutor().execute(command);
+      GetDocumentsResult commandResult = command.getResult();
+
+      if (commandResult.getResults().size() > 0) {
+        JsonNode json = commandResult.getResults().get(0);
+        fillMap(json, result, fields);
+        return Status.OK;
+      } else {
+        return Status.NOT_FOUND;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  private void fillMap(JsonNode json, HashMap<String, ByteIterator> result, Set<String> fields) {
+    boolean checkFields = fields != null && !fields.isEmpty();
+    for (Iterator<Map.Entry<String, JsonNode>> jsonFields = json.fields();
+         jsonFields.hasNext();
+      /* increment in loop body */) {
+
+      Map.Entry<String, JsonNode> jsonField = jsonFields.next();
+      String name = jsonField.getKey();
+      if ((checkFields && !fields.contains(name)) || Constants.Documents.Metadata.KEY.equals(name)) {
+        continue;
+      }
+      JsonNode jsonValue = jsonField.getValue();
+      if (jsonValue != null && !jsonValue.isNull()) {
+        result.put(name, new StringByteIterator(jsonValue.asText()));
+      }
+    }
   }
 
   @Override
   public Status scan(String table, String startkey, int recordcount, Set<String> fields,
                      Vector<HashMap<String, ByteIterator>> result) {
-    return null;
+    StringBuilder queryBuilder = new StringBuilder();
+
+    try {
+      String documentId = documentId(table, startkey);
+
+      //TODO: client vs server projection
+      queryBuilder.append("from '")
+          .append(table)
+          .append("' where id() >= '")
+          .append(documentId)
+          .append("'");
+
+      IndexQuery indexQuery = new IndexQuery(queryBuilder.toString());
+      indexQuery.setPageSize(recordcount);
+      QueryCommand queryCommand = new QueryCommand(store.getConventions(), indexQuery, false, false);
+
+      store.getRequestExecutor().execute(queryCommand);
+
+      ArrayNode results = queryCommand.getResult().getResults();
+      if (results.size() == 0) {
+        return Status.ERROR;
+      }
+
+      result.ensureCapacity(recordcount);
+
+      for (int i = 0; i < results.size(); i++) {
+        JsonNode json = results.get(i);
+        HashMap<String, ByteIterator> resultMap =
+            new HashMap<String, ByteIterator>();
+
+        fillMap(json, resultMap, fields);
+        result.add(resultMap);
+      }
+      return Status.OK;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
   }
 
   @Override
   public Status update(String table, String key, HashMap<String, ByteIterator> values) {
-    return null;
+    //TODO: replace all?
+    return insert(table, key, values);
   }
 
   @Override
   public Status insert(String table, String key, HashMap<String, ByteIterator> values) {
-    return null;
+    String documentId = documentId(table, key);
+
+    //TODO: support for batches?
+    try {
+      ObjectNode node = toJson(values);
+      ObjectNode metadata = mapper.createObjectNode();
+      metadata.set(Constants.Documents.Metadata.COLLECTION, mapper.valueToTree(table));
+      node.set(Constants.Documents.Metadata.KEY, metadata);
+
+      PutDocumentCommand command = new PutDocumentCommand(documentId, null, node);
+      store.getRequestExecutor().execute(command);
+
+      return Status.OK;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
   }
 
   @Override
   public Status delete(String table, String key) {
-    return null;
+    String documentId = documentId(table, key);
+
+    try {
+      DeleteDocumentCommand command = new DeleteDocumentCommand(documentId);
+      store.getRequestExecutor().execute(command);
+      return Status.OK;
+
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  protected static ObjectNode toJson(Map<String, ByteIterator> values)
+      throws IOException {
+    ObjectNode node = mapper.createObjectNode();
+    HashMap<String, String> stringMap = StringByteIterator.getStringMap(values);
+    for (Map.Entry<String, String> pair : stringMap.entrySet()) {
+      node.put(pair.getKey(), pair.getValue());
+    }
+
+    return node;
   }
 }
--- ravendb/src/test/java/com/yahoo/ycsb/db/RavenDBClientTest.java
+++ ravendb/src/test/java/com/yahoo/ycsb/db/RavenDBClientTest.java
@@ -1,24 +1,51 @@
 package com.yahoo.ycsb.db;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.Status;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import com.yahoo.ycsb.StringByteIterator;
+import net.ravendb.client.Constants;
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.operations.DeleteByQueryOperation;
+import net.ravendb.client.documents.queries.IndexQuery;
+import net.ravendb.client.documents.queries.Query;
+import net.ravendb.client.documents.session.IDocumentSession;
+import org.junit.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.SQLException;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class RavenDBClientTest {
 
-  private RavenDBClient client;
+  private static final String TEST_RAVEN_URL = "http://localhost:8080";
+  private static final String TEST_RAVEN_DATABASE = "test";
+  private static final String TABLE_NAME = "USERTABLE";
+  private static final int FIELD_LENGTH = 32;
+  private static final String FIELD_PREFIX = "FIELD";
+  private static final String KEY_PREFIX = "user";
+  private static final String KEY_FIELD = "YCSB_KEY";
+  private static RavenDBClient client;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    client = new RavenDBClient();
+
+    Properties p = new Properties();
+    p.setProperty(RavenDBClient.RAVENDB_URL, TEST_RAVEN_URL);
+    p.setProperty(RavenDBClient.RAVENDB_DATABASE, TEST_RAVEN_DATABASE);
+
+    client.setProperties(p);
+    client.init();
+  }
 
   @Before
-  public void setup() throws Exception {
-    // tODO set up your client
-
-    client = new RavenDBClient();
-    client.init();
+  public void prepareTest() {
+    DocumentStore store = RavenDBClient.getStore();
+    IndexQuery query = new IndexQuery("from @all_docs");
+    DeleteByQueryOperation operation = new DeleteByQueryOperation(query);
+    store.operations().sendAsync(operation).waitForCompletion();
   }
 
   @Test
@@ -32,12 +59,156 @@ public class RavenDBClientTest {
     Assert.assertEquals(Status.OK, result);
   }
 
-  @After
-  public void cleanup() throws Exception {
-    // TODO: clean up method
-
-    client.cleanup();
+  @AfterClass
+  public static void teardown() {
+    if (client != null) {
+      try {
+        client.cleanup();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
   }
 
+  /*
+        This is a copy of buildDeterministicValue() from core:com.yahoo.ycsb.workloads.CoreWorkload.java.
+        That method is neither public nor static so we need a copy.
+     */
+  private String buildDeterministicValue(String key, String fieldkey) {
+    int size = FIELD_LENGTH;
+    StringBuilder sb = new StringBuilder(size);
+    sb.append(key);
+    sb.append(':');
+    sb.append(fieldkey);
+    while (sb.length() < size) {
+      sb.append(':');
+      sb.append(sb.toString().hashCode());
+    }
+    sb.setLength(size);
+
+    return sb.toString();
+  }
+
+  private HashMap<String, ByteIterator> insertRow(String insertKey) {
+    HashMap<String, ByteIterator> insertMap = new HashMap<String, ByteIterator>();
+    for (int i = 0; i < 3; i++) {
+      insertMap.put(FIELD_PREFIX + i, new StringByteIterator(buildDeterministicValue(insertKey, FIELD_PREFIX + i)));
+    }
+    client.insert(TABLE_NAME, insertKey, insertMap);
+
+    return insertMap;
+  }
+
+  @Test
+  public void insertTest() {
+    String insertKey = "user0";
+    HashMap<String, ByteIterator> insertMap = insertRow(insertKey);
+
+    try (IDocumentSession session = RavenDBClient.getStore().openSession()) {
+      List<ObjectNode> result = session.query(ObjectNode.class, Query.collection(TABLE_NAME))
+          .waitForNonStaleResults()
+          .toList();
+
+      // Check we have a result Row
+      assertEquals(1, result.size());
+      // Check that all the columns have expected values
+      assertEquals(TABLE_NAME + "/" + insertKey,
+          result
+              .get(0)
+              .get(Constants.Documents.Metadata.KEY)
+              .get(Constants.Documents.Metadata.ID)
+              .textValue());
+      for (int i = 0; i < 3; i++) {
+        assertEquals(result.get(0).get(FIELD_PREFIX + i).textValue(), insertMap.get(FIELD_PREFIX + i).toString());
+      }
+    }
+  }
+
+  // TODO: test for update?
+
+  @Test
+  public void readTest() {
+    String insertKey = "user0";
+    HashMap<String, ByteIterator> insertMap = insertRow(insertKey);
+    HashSet<String> readFields = new HashSet<String>();
+    HashMap<String, ByteIterator> readResultMap = new HashMap<String, ByteIterator>();
+
+    // Test reading a single field
+    readFields.add("FIELD0");
+    client.read(TABLE_NAME, insertKey, readFields, readResultMap);
+    assertEquals("Assert that result has correct number of fields", readFields.size(), readResultMap.size());
+    for (String field: readFields) {
+      assertEquals("Assert " + field + " was read correctly", insertMap.get(field).toString(), readResultMap.get(field).toString());
+    }
+
+    readResultMap = new HashMap<String, ByteIterator>();
+
+    // Test reading all fields
+    readFields.add("FIELD1");
+    readFields.add("FIELD2");
+    client.read(TABLE_NAME, insertKey, readFields, readResultMap);
+    assertEquals("Assert that result has correct number of fields", readFields.size(), readResultMap.size());
+    for (String field: readFields) {
+      assertEquals("Assert " + field + " was read correctly", insertMap.get(field).toString(), readResultMap.get(field).toString());
+    }
+  }
+
+  @Test
+  public void deleteTest() {
+    insertRow("user0");
+    String deleteKey = "user1";
+    insertRow(deleteKey);
+    insertRow("user2");
+
+    client.delete(TABLE_NAME, deleteKey);
+
+    try (IDocumentSession session = RavenDBClient.getStore().openSession()) {
+      List<ObjectNode> objectNodes = session.query(ObjectNode.class, Query.collection(TABLE_NAME))
+          .toList();
+
+      int totalRows = 0;
+      for (ObjectNode objectNode : objectNodes) {
+        String id = objectNode.get(Constants.Documents.Metadata.KEY)
+            .get(Constants.Documents.Metadata.ID)
+            .textValue();
+
+        assertNotEquals("Assert this is not the deleted row key", deleteKey, id);
+        totalRows++;
+      }
+
+      assertEquals("Assert we ended with the correct number of rows", totalRows, 2);
+    }
+  }
+
+  @Test
+  public void scanTest() throws SQLException {
+    HashMap<String, HashMap<String, ByteIterator>> keyMap = new HashMap<String, HashMap<String, ByteIterator>>();
+    for (int i = 0; i < 5; i++) {
+      String insertKey = KEY_PREFIX + i;
+      keyMap.put(insertKey, insertRow(insertKey));
+    }
+    HashSet<String> fieldSet = new HashSet<String>();
+    fieldSet.add("FIELD0");
+    fieldSet.add("FIELD1");
+    int startIndex = 1;
+    int resultRows = 3;
+
+    Vector<HashMap<String, ByteIterator>> resultVector = new Vector<HashMap<String, ByteIterator>>();
+    client.scan(TABLE_NAME, KEY_PREFIX + startIndex, resultRows, fieldSet, resultVector);
+
+    // Check the resultVector is the correct size
+    assertEquals("Assert the correct number of results rows were returned", resultRows, resultVector.size());
+    // Check each vector row to make sure we have the correct fields
+    int testIndex = startIndex;
+    for (HashMap<String, ByteIterator> result: resultVector) {
+      assertEquals("Assert that this row has the correct number of fields", fieldSet.size(), result.size());
+      for (String field: fieldSet) {
+        assertEquals("Assert this field is correct in this row", keyMap.get(KEY_PREFIX + testIndex).get(field).toString(), result.get(field).toString());
+      }
+      testIndex++;
+    }
+  }
+
+  //TODO: batch tests
 
 }
